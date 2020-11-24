727509e2d235d78f145fcf731e244216064c66c7
Jackson Rodrigo Alnoch <jackalnoch@gmail.com>
2020-11-24 19:38:23 -0400

    Atualização pagina footer.php

---
+++ footer.php
@@ -0,0 +1,47 @@
+<footer>
+        <div class="footer">
+            <div class="footer-left">
+                <p class="about">
+                    A criação de uma fundação municipal de cultura pode ser uma alternativa eficaz para a implantação de
+                    políticas culturais que correspondem as aspirações da sociedade, o autor aborda os cuidados; os
+                    resultados e as experiências. Com o objetivo de promover, incentivar, difundir e valorizar a cultura
+                    e as artes na cidade.
+                </p>
+            </div>
+            <div class="footer-right">
+                <div>
+                    <ft>Contato</ft>
+                    <i class="fas fa-compass"></i>
+                    <p>
+                        Av. Tancredo Neves - n° 3845
+                        <span>Vilhena, Rondônia - 76980-731</span>
+                    </p>
+                </div>
+                <div>
+                    <i class="fa fa-phone"></i>
+                    <p>(69) 3321-1777</p>
+                </div>
+                <div>
+                    <i class="fa fa-envelope"></i>
+                    <p><a href="mailto:fundacaoculturaldevilhena@gmail.com">fundacaoculturaldevilhena@gmail.com</a></p>
+                </div>
+                <hr class="horizontal">
+                <div class="social-media">
+                    <ft>Redes Sociais</ft>
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                    <a href="#"><i class="fa fa-youtube"></i></a>
+                </div>
+                <hr class="horizontal">
+                <div class="partners">
+                    <ft>Parceiros</ft>
+                    <img class="partner" src="assets/image/vortex_logo.jpeg" width="50" height="50">
+                    <img class="partner" src="assets/image/fslab_logo.jpeg" width="50" height="50">
+                    <img class="partner" src="assets/image/ifro_logo.jpeg" width="50" height="50">
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
