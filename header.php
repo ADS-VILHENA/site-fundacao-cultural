0dc8f3ff60b4d204b80293ce5dd7a48a8e6542e5
Jackson Rodrigo Alnoch <jackalnoch@gmail.com>
2020-11-24 19:35:40 -0400

    Atualizando pagina header.php

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<link rel="shortcut icon" href="favicon.png">
+
+<head>
+    <title>Fundação Cultural de Vilhena</title>
+    <!--Lembre-se de alterar o título da página.-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/style.css">
+    <script src="https://kit.fontawesome.com/f148631463.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <header>
+        <nav class="header">
+            <img src="assets/image/logo.png" width="125" alt="Fundação Cultural de Vilhena" class="logo">
+            <input type="checkbox" id="check">
+            <label for="check" class="show-menu"><i class="fas fa-bars"></i></label>
+            <div class="menu">
+                <li><a href="#">Início</a></li>
+                <li><a href="#">Sobre nós</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Contato</a></li>
+                <label for="check" class="hide-menu"><i class="fas fa-times"></i></label>
+            </div>
+            <li class="search-bar">
+                <input type="search" placeholder="Pesquisar" required>
+                <label for="check" class="search-button"><i class="fas fa-search"></i></label>
+            </li>
+        </nav>
+    </header>
