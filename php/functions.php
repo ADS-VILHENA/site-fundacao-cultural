f4739647b0d39bc6c21cc205f710f993659df9b1
unknown <erickymorenoads@gmail.com>
2020-12-06 14:33:06 -0400

    Desenvolvendo pages home em php

--- php/functions.php
+++ php/functions.php
@@ -1,13 +1,16 @@
 <?php
 
     function fc_wp_styles(){
-        wp_enqueue_style('style_css', get_stylesheet_uri();
+        wp_enqueue_style('style_css1', get_stylesheet_directory_uri() . '/assets/style.css');
         wp_enqueue_style('font_mont','https://fonts.googleapis.com/css?family=Montserrat');
         wp_enqueue_style('fonts_jose','https://fonts.googleapis.com/css?family=Josefin+Sans');
     }
 
     add_action('wp_enqueue_style','fc_wp_styles');
+    add_action('wp_enqueue_scripts', 'fc_wp_styles');
 
     add_theme_support('title-tag');
 
     add_theme_support('post-thumbnails');
+
+?>
--- php/index.php
+++ php/index.php
@@ -1,26 +1,28 @@
 <?php get_header(); ?>
 
-    <main>
+<main>
         <div class="main">
-            <div class="content" style="display:flex;">
-
-                <?php while (have_posts()) : if have_posts() : the_posts(); ?> 
-
-                <div class="noticias" style="width: 100%; margin-right: 30px; position: relative; display:block;">
-                <a href="<?php the_permalink();?>">
-                    <img src="./assets/image/Expovil - 55.jpg" alt="Foto noticia" style="width: 100%;"><br></br>
-                        <h1 style="width: 100%; margin-right: 20px; text-align: center;">Feira agropecuária: Iª noite de rodeio</h1><br></br>
-                        <p style="width: 100%; margin-right: 20px; text-align:justify;">
-                            Pretium aenean pharetra magna ac placerat vestibulum lectus mauris ultrices. Imperdiet nulla malesuada pellentesque elit eget. Arcu cursus euismod quis viverra nibh cras.
-                        </p>
-                        <h3 style="width: 100%; margin-right: 20px; text-align: center;">Recentes</h3>
-                </a>
+            <div class="content">
+                <div class="news">
+                    <a href="<?php the_permalink();?>">
+                    <img src="./assets/image/Expovil - 55.jpg" alt="Foto notícia">
+                    <h1>Feira agropecuária: Iª noite de
+                        rodeio</h1>
+                    <p>
+                        Pretium aenean pharetra magna ac placerat vestibulum lectus mauris ultrices. Imperdiet nulla
+                        malesuada pellentesque elit eget. Arcu cursus euismod quis viverra nibh cras.
+                    </p>
+                    <hr class="horizontal">
+                    <h1>Recentes</h1>
+                    </a>
                 </div>
-                    <div class="classes" style="width: 80%; position: relative; display:block;">
+                <div class="segments">
+                    <div class="index_segment">
                         <?php the_post_thumbnail('full');?>
-                        <h1 style="width: 80%; text-align: center;"><?php the_title(); ?></h1>
+                        <h1><?php the_title(); ?></h1>
                     </div>
-                    
+                </div>
+
             </div>
 
         </div>
