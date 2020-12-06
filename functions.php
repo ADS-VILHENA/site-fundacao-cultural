c597c827953f6aca8d24c90aab7c5c295569aa86
unknown <erickymorenoads@gmail.com>
2020-12-06 19:37:53 -0400

    Atualizando paginas php

--- functions.php
+++ functions.php
@@ -5,10 +5,14 @@
         wp_enqueue_style('font_mont','https://fonts.googleapis.com/css?family=Montserrat');
         wp_enqueue_style('fonts_jose','https://fonts.googleapis.com/css?family=Josefin+Sans');
     }
-
     add_action('wp_enqueue_style','fc_wp_styles');
     add_action('wp_enqueue_scripts', 'fc_wp_styles');
 
+    function fc_adicionar_recursos(){
+        add_theme_support('custom-logo');
+    }
+    add_action('after_setup_theme','fc_adicionar_recursos');
+    
     add_theme_support('title-tag');
 
     add_theme_support('post-thumbnails');
--- header.php
+++ header.php
@@ -6,15 +6,16 @@
     
     <meta charset="<?php bloginfo('charset');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('name') ?></title>
     
     <script src="https://kit.fontawesome.com/f148631463.js" crossorigin="anonymous"></script>
     <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class()?>>
     <header>
         <nav class="header">
-            <a href <?php echo home_url ('/'); ?>><img src="assets/image/logo.png" width="125" alt="Fundação Cultural de Vilhena" class="logo"></a>
+            <a href <?php echo home_url ('/'); ?>class="logo"><?php the_custom_logo()?></a>
             <input type="checkbox" id="check">
             <label for="check" class="show-menu"><i class="fas fa-bars"></i></label>
             <div class="menu">
